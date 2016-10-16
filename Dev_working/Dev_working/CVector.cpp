e2fc90ef90e2bd9143c3b78377a32bae3602db86
Razmik Karapetyan <karapetyanraz95@gmail.com>
2016-10-16 14:55:56 +0400

    Create CIntData,CDoubleData,CStringData,CDateTimeData classes.

---
+++ Dev_working/Dev_working/CVector.cpp
@@ -0,0 +1,83 @@
+#include "CVector.h"
+#include <stdexcept>
+
+// 
+// CIntData implementation
+//
+CIntData::CIntData() : m_arr_data() {}
+
+CIntData::CIntData(QVector<int> arr_data) : m_arr_data(arr_data) {}
+
+int  CIntData::GetAt(int index) const
+{
+	if (index >= m_arr_data.size() || index < 0)
+		throw std::out_of_range("Going out of range of array !\n");
+	else
+		return m_arr_data[index];
+}
+
+int CIntData::GetCount() const
+{
+	return m_arr_data.size();
+}
+
+
+// 
+// CDoubleData implementation
+//
+CDoubleData::CDoubleData() : m_arr_data() {}
+
+CDoubleData::CDoubleData(QVector<double> arr_data) : m_arr_data(arr_data) {}
+
+double  CDoubleData::GetAt(int index) const
+{
+	if (index >= m_arr_data.size() || index < 0)
+		throw std::out_of_range("Going out of range of array !\n");
+	else
+		return m_arr_data[index];
+}
+
+int CDoubleData::GetCount() const
+{
+	return m_arr_data.size();
+}
+
+//
+//CStringData implementation
+//
+CStringData::CStringData() : m_arr_data() {}
+
+CStringData::CStringData(QVector<QString> arr_data) : m_arr_data(arr_data) {}
+
+QString  CStringData::GetAt(int index) const
+{
+	if (index >= m_arr_data.size() || index < 0)
+		throw std::out_of_range("Going out of range of array !\n");
+	else
+		return m_arr_data[index];
+}
+
+int CStringData::GetCount() const
+{
+	return m_arr_data.size();
+}
+
+//
+// CDateTimeData implementation
+//
+CDateTimeData::CDateTimeData() : m_arr_data() {}
+
+CDateTimeData::CDateTimeData(QVector<QDateTime> arr_data) : m_arr_data(arr_data) {}
+
+QDateTime  CDateTimeData::GetAt(int index) const
+{
+	if (index >= m_arr_data.size() || index < 0)
+		throw std::out_of_range("Going out of range of array !\n");
+	else
+		return m_arr_data[index];
+}
+
+int CDateTimeData::GetCount() const
+{
+	return m_arr_data.size();
+}
---
+++ Dev_working/Dev_working/CVector.h
@@ -0,0 +1,66 @@
+#ifndef __CVECTOR___
+#define __CVECTOR___
+
+#include <QVector>
+#include <QString>
+#include <QDateTime>
+#include "IField_collection.h"
+
+// 
+// CIntData declaration
+//
+class CIntData :public IIntVector
+{
+public:
+	CIntData();
+	CIntData(QVector<int>);
+	int GetAt(int) const override;
+	int GetCount() const override;
+private:
+	QVector<int> m_arr_data;
+};
+
+// 
+// CDoubleData declaration
+//
+class CDoubleData : public IDoubleVector
+{
+public:
+	CDoubleData();
+	CDoubleData(QVector<double>);
+	double GetAt(int) const override;
+	int GetCount() const override;
+private:
+	QVector<double> m_arr_data;
+};
+
+// 
+// CStringData declaration
+//
+class CStringData : public IStringVector
+{
+public: 
+	CStringData();
+	CStringData(QVector<QString>);
+	QString GetAt(int) const override;
+	int GetCount() const override;
+private:
+	QVector<QString> m_arr_data;
+};
+
+//
+// CDateTimeData declaration
+//
+class CDateTimeData : public IDateTimeVector
+{
+public:
+	CDateTimeData();
+	CDateTimeData(QVector<QDateTime>);
+	QDateTime GetAt(int) const override;
+	int GetCount() const override;
+private:
+	QVector<QDateTime> m_arr_data;
+};
+
+
+#endif //__CVECTOR___
