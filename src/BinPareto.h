6d59772031da1391011737b46738e193e303157c
TamaraSmbatyan <smbatyantamara95@gmail.com>
2016-10-20 00:04:59 +0400

    Create BinPareto.h

---
+++ src/BinPareto.h
@@ -0,0 +1,18 @@
+#ifndef BinPareto_H
+#define BinPareto_H
+
+class BinPareto :public IAnalyser
+{
+public:
+	BinPareto();
+	void setConfig(const Configuration &);
+	void setData(const DataSelection &);
+	QWidget *getView()const;
+	void run()const;
+	~BinPareto();
+private:
+	Configuration m_config;
+	DataSelection m_data;
+};
+
+#endif //BinPareto_H
