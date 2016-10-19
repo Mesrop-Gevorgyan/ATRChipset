8f34a23b0a1053506a84ee8d3d2329671fdf81d5
TamaraSmbatyan <smbatyantamara95@gmail.com>
2016-10-20 00:06:16 +0400

    Create BinPareto.cpp

---
+++ src/BinPareto.cpp
@@ -0,0 +1,27 @@
+#include "BinPareto.h"
+
+BinPareto::BinPAreto()
+{}
+
+void BinPareto::setConfig(const Configuration & config)
+{
+	m_config = config;
+}
+
+void BinPareto::setData(const DataSelection &)
+{
+	m_data = data;
+}
+
+QWidget * BinPareto::getView()const
+{
+
+}
+
+void BinPareto::run()const
+{
+
+}
+
+BinPAreto::~BinPareto()
+{}
