083054ace1aeccb753a9d5f45de0d2b227e03da3
Seda <seda-matevosyan-1996@mail.ru>
2016-11-16 00:05:05 +0400

    Add files via upload

--- src/dialog.cpp
+++ src/dialog.cpp
@@ -1,6 +1,6 @@
 #include "dialog.h"
 #include "ui_dialog.h"
-
+#include <QCheckBox>
 
 Dialog::Dialog(QWidget *parent) :
     QDialog(parent),
@@ -32,47 +32,30 @@ Dialog::Dialog(QWidget *parent) :
      ui->label_6->setFont(font);
      ui->label_6->setStyleSheet("QLabel {color : #007fff; }");
 
-    ui->comboBox->addItem("   Yield");
-    ui->comboBox->addItem("   Parameter Summary");
-    ui->comboBox->addItem("   Bin Summary");
+     ui->comboBox->addItem("   Yield");
+     ui->comboBox->addItem("   Parameter Summary");
+     ui->comboBox->addItem("   Bin Summary");
 
-    QCheckBox * pchb = new QCheckBox();
-    pchb->setText("Lot");
+     ui->checkBox->setFont(font);
+     ui->checkBox->setStyleSheet("QCheckBox {color : #007fff; }");
+     ui->checkBox_2->setFont(font);
+     ui->checkBox_2->setStyleSheet("QCheckBox {color : #007fff; }");
+     ui->checkBox_3->setFont(font);
+     ui->checkBox_3->setStyleSheet("QCheckBox {color : #007fff; }");
 
-    ui->comboBox_2->addItem("   Lot");
-    ui->comboBox_2->addItem("   Wafer");
-    ui->comboBox_2->addItem("   Device");
-
-
-    font.setPointSize(14);
-    font.setBold(false);
-    ui->checkBox->setFont(font);
-    ui->checkBox->setStyleSheet("QCheckBox {color : #007fff; }");
-    ui->checkBox_2->setFont(font);
-    ui->checkBox_2->setStyleSheet("QCheckBox {color : #007fff; }");
-    ui->checkBox_3->setFont(font);
-    ui->checkBox_3->setStyleSheet("QCheckBox {color : #007fff; }");
-    ui->checkBox_4->setFont(font);
-    ui->checkBox_4->setStyleSheet("QCheckBox {color : #007fff; }");
-    ui->checkBox_5->setFont(font);
-    ui->checkBox_5->setStyleSheet("QCheckBox {color : #007fff; }");
-    ui->checkBox_6->setFont(font);
-    ui->checkBox_6->setStyleSheet("QCheckBox {color : #007fff; }");
-    ui->checkBox_7->setFont(font);
-    ui->checkBox_7->setStyleSheet("QCheckBox {color : #007fff; }");
-    ui->checkBox_8->setFont(font);
-    ui->checkBox_8->setStyleSheet("QCheckBox {color : #007fff; }");
-    ui->checkBox_9->setFont(font);
-    ui->checkBox_9->setStyleSheet("QCheckBox {color : #007fff; }");
-
+     QCheckBox * pchb = new QCheckBox();
+     pchb->setText("Lot");
 }
 
 Dialog::~Dialog()
 {
     delete ui;
 }
+#include <iostream>
 void Dialog::on_comboBox_currentIndexChanged(const QString &arg1)
 {
+    QVector<QString> choices;
+    std::cout<<arg1.toStdString()<<std::endl;
    //for (int i = 0; i< CheckBoxVec.size(); ++i)
     //{
         //CheckBoxVec[i]->hide();
@@ -81,8 +64,25 @@ void Dialog::on_comboBox_currentIndexChanged(const QString &arg1)
     //CheckBoxVec.resize(0);
     //std::cout<<CheckBoxVec.size()<<std::endl;
 
-    //if (arg1 == "   Yield")
-    //{
+    if (arg1 == "   Yield" || arg1 == "   Bin Summary")
+    {
+        QVector<QString> current_choices;
+        current_choices.push_back("HBIN");
+        current_choices.push_back("SBIN");
+        choices = current_choices;
+    }
+    else
+    {
+        QVector<QString> current_choices;
+        current_choices.push_back("Min");
+        current_choices.push_back("Max");
+        current_choices.push_back("Median");
+        current_choices.push_back("Average");
+        current_choices.push_back("Standard deviation");
+        current_choices.push_back("Skewness");
+        current_choices.push_back("Kurtosis");
+        choices = current_choices;
+    }
         //QCheckBox * pchb = new QCheckBox();
         //pchb->setText("HBIN");
         //QCheckBox * pchb_1 = new QCheckBox();
@@ -95,13 +95,37 @@ void Dialog::on_comboBox_currentIndexChanged(const QString &arg1)
     //}
     //std::cout<<CheckBoxVec.size()<<std::endl;
 
-    if (arg1 == "   Yield" || arg1 == "   Bin Summary")
+    /*if (arg1 == "   Yield" || arg1 == "   Bin Summary")
     {
         //HBIN and SBIN
     }
     else
     {
         //MIN MAX MEDIAN ...
+    }*/
+
+    //remove layout contant
+    QLayoutItem *child;
+    while ((child = ui->verticalLayout->takeAt(0)) != 0)  {
+        delete child->widget();
     }
 
+    /*QList<QWidget *> widgets = ui->verticalLayout->findChildren<QWidget *>();
+    foreach(QWidget * child, widgets)
+    {
+        ui->verticalLayout->removeWidget(child);
+        delete child;
+    }*/
+
+    QFont font = ui->label->font();
+    font.setPointSize(16);
+    font.setBold(false);
+    for (int i=0; i<choices.size(); ++i)
+    {
+        QCheckBox * c_b = new QCheckBox(choices[i],this);
+        c_b->setFont(font);
+        c_b->setStyleSheet("QCheckBox {color : #007fff; }");
+        std::cout<<choices[i].toStdString()<<std::endl;
+        ui->verticalLayout->addWidget(c_b);
+    }
 }
