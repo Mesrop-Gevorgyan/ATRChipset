972aa25d758442526663533cb0fe7e8fbaf78751
Mesrop-Gevorgyan <mesrop_gevorgyan_7@mail.ru>
2016-12-06 10:15:11 +0400

    Update idatadirectory.h

--- src/Interfaces/idatadirectory.h
+++ src/Interfaces/idatadirectory.h
@@ -20,7 +20,7 @@ public:
 	// Get field list of all field types
 	virtual FieldList getFields() const = 0;
 	// Get values as QVariant of field
-	virtual QVariantList getFieldValues() const = 0;
+	virtual QVariantList getFieldValues(Field const& oID) const = 0;
 	//Get information corresponding files
 	virtual QList<FileInfo> getFiles(const QList<FileType>&)const = 0;
 };
