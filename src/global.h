14bea084644429176a4ce3942bcfde1d37d10851
RazKarapetyan <karapetyanraz95@gmail.com>
2017-01-19 23:24:44 +0400

    Add files via upload

--- src/global.h
+++ src/global.h
@@ -1,24 +1,38 @@
-
-
 #ifndef GLOBAL_H
 #define GLOBAL_H
-
-
 
 // Forword declaration
 class QString;
 class QStringList;
+class QDateTime;
+class FileContext;
+
+// Represent Field type in FileContext directory 
+enum FieldType
+{
+        Context,
+        TestCondition,
+};
 
 
 // Field ID
 typedef QString Field;
 typedef QStringList FieldList;
-
+typedef QVariant Value;
+typedef QPair<Value, FieldType>  FieldValue;
 
 // Bin type
 enum class BinType { HBin, SBin };
 
-
+// Types of files,which will be used
+enum FileType
+{
+        BinData,
+        BinDefinition,
+        ParameterDefinition,
+        ParameterData,
+        ParameterLimits,
+};
 
 #endif // !GLOBAL_H
 
