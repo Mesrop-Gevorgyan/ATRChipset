bd9e8d597f2e4c1255d99f2491014d27f355854b
Anna-Vorskanyan <annavorskanyan@yahoo.com>
2017-01-25 22:29:32 +0400

    Update DataDirectory.h

--- src/DataDirectory.h
+++ src/DataDirectory.h
@@ -13,6 +13,10 @@ public:
     FieldList getFields()const;
     QVariantList getFieldValues(Field const& oID)const;
     QList<FileInfo> getFiles(const QSet<FileType>& fileTypes, const QList<FileContext> &contextList)const;
+    QSet<QString> getWafers()const;
+    QSet<QString> getDevices()const;
+    QSet<QString> getLots()const;
+    QSet<QDateTime> getDates()const;
 private:
     QMultiMap<FileType,FileInfo> m_type_file;
 
@@ -22,3 +26,4 @@ private:
 
 
 #endif // DATADIRECTORY_H
+
