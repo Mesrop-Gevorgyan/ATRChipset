e895c6e1531d3eeda6b611d8bb0c1465009acfa2
Mesrop-Gevorgyan <mesrop_gevorgyan_7@mail.ru>
2017-02-22 20:38:25 +0400

    Add Axis class (primary)

---
+++ src/Wafer/wm_axis.cpp
@@ -0,0 +1,24 @@
+
+
+// Includes
+#include "wm_axis.h"
+
+// Qt includs
+#include <QtCore\QRect>
+#include <QtGui\QPainter>
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Imlementation of CAxis
+//
+QRect wm::CAxis::doLayout( QRect const& rc )
+{
+	return QRect();
+}
+
+void wm::CAxis::draw( QPainter* pPainter ) const
+{
+	
+}
+///////////////////////////////////////////////////////////////////////////////
