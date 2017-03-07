0128144320c6902652e3e3664a48229834fbad9f
TamaraSmbatyan <smbatyantamara95@gmail.com>
2017-03-07 15:35:13 +0400

    Update and rename IAnalyser.h to ianalyser.h

---
+++ src/Interfaces/ianalyser.h
@@ -0,0 +1,21 @@
+#ifndef IANALYSER_H
+#define IANALYSER_H
+
+#include "configuration.h"
+#include "selection.h"
+#include <QWidget>
+
+class IAnalyser
+{
+public:
+    //set configuration
+    virtual void setConfig(const CConfiguration &) = 0;
+    //set data selection
+    virtual void setData(const CSelection &) = 0;
+    //shows the window of result
+    QWidget *getView()const = 0;
+    //runs the analyse
+    void run()const = 0;
+};
+
+#endif //IANALYSER_H
