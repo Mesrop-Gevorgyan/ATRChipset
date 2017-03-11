d62d4ff30d007e61ed7ad9ed891dd2557e64baad
Anna-Vorskanyan <annavorskanyan@yahoo.com>
2017-03-12 01:03:53 +0400

    Update loader.h

--- src/loader.h
+++ src/loader.h
@@ -3,13 +3,13 @@
 
 #include "DataDirectory.h"
 #include "DataStore.h"
-
+#include "FileInfoList.h"
 class loader
 {
 public:
     loader(DataStore* ptrDataStore);
     //loaded data from files to datastore
-    void loadData(const QList<QString>& fileList);
+    void loadData(const CFileInfoList& fileList);
 private:
     DataStore* pDataStore;
 };
