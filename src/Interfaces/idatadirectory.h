c6fcd881ec826a6d82d9f6b92bc4f40f7011b0c7
Anna-Vorskanyan <annavorskanyan@yahoo.com>
2017-03-12 00:59:57 +0400

    Update idatadirectory.h

--- src/Interfaces/idatadirectory.h
+++ src/Interfaces/idatadirectory.h
@@ -1,28 +1,22 @@
 #ifndef __IDATADIRECTORY__
 #define __IDATADIRECTORY__
 
+
 #include <QVariant>
-#include "global.h"
+//class QVariantList;
 #include "DataStore.h"
 #include "FileInfoList.h"
+#include "DataIndex.h"
+
+typedef  QStringList FieldList;
 
 class IDataDirectory
 {
 public:
-    // Get field list of all field types
-    virtual FieldList getFields() const = 0;
-    // Get values as QVariant of field
     virtual QVariantList getFieldValues(Field const& oID) const = 0;
-    //Get information corresponding files
-    virtual CFileInfoList getFiles(const QSet<FileType>&,const CFileInfoList &) const = 0;
-    //Get wafer names
-    virtual QSet<QString> getWafers() const = 0;
-    //Get device name
-    virtual QSet<QString> getDevices() const = 0;
-    //Get Lot name
-    virtual QSet<QString> getLots() const = 0;
-    //Get dates
-    virtual QSet<QDateTime> getDates()const = 0;
+    virtual IDList GetIDList(QStringList pattern) = 0;
+    virtual FileInfo GetFileInfo(ID id)  =  0;
+    virtual FieldList GetFieldList(QStringList pattern, Field field)  = 0;
 };
 
 #endif //__IDATADIRECTORY__
