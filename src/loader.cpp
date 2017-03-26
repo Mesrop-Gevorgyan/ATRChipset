0fa77e69a9f5b9efa66b35dab586e1e33b64d454
Anna-Vorskanyan <annavorskanyan@yahoo.com>
2017-03-26 23:28:12 +0400

    Update loader.cpp

--- src/loader.cpp
+++ src/loader.cpp
@@ -1,17 +1,15 @@
 #include "loader.h"
 
-loader::loader(DataStore *ptrDataStore):pDataStore(ptrDataStore)
+loader::loader(DataStorePtr ptrDataStore):pDataStore(ptrDataStore)
 {
 }
 
-void loader::loadData(const CFileInfoList &fileList)
+void loader::loadData(const CFileInfoList &fileInfoList)
 {
-    for(int i = 0;i < fileList.count(); ++i )
+    for(int i = 0; i < fileInfoList.count(); ++i )
     {
-        parser Parser(fileList[i].m_filePath);
-        CVectorCollection data = Parser.loader(fileList[i].m_fileType);
-        pDataStore->add(fileList[i].ID,data);
+        parser Parser(fileInfoList[i].m_filePath);
+        FileData data = Parser.loader(fileInfoList[i].m_fileType);
+        pDataStore->add(fileInfoList[i].ID,data);
     }
 }
-
-
