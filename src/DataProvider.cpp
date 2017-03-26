67a96b5d64093e6de7c7c80b2f2d7077c0bc1b9d
Anna-Vorskanyan <annavorskanyan@yahoo.com>
2017-03-26 23:03:12 +0400

    Update DataProvider.cpp

--- src/DataProvider.cpp
+++ src/DataProvider.cpp
@@ -1,59 +1,24 @@
 #include "DataProvider.h"
 
-void DataProvider::init(const CSelection& oSelection, IDataDirectory* pDataDir)
+void DataProvider::init(CSelection const& oSelection,IDataDirectoryPtr pDataDir)
 {
-	pDataDirectory = pDataDir;
-	m_selection = oSelection;
+    m_selection = oSelection;
+    pDataDirectory = pDataDir;
 }
 
-ITablePtr DataProvider::GetData()
+ITablePtr DataProvider::GetData(const FieldList&)
 {
-        CFileInfoList fileInfo = getContextsList(); //contexts is CfileInfoList
+        const IDList& idList = pDataDirectory->GetIDList(m_selection);
         CFileInfoList FilesForLoad;
         loader load(&m_dataStore);
-        for(int i = 0; i < fileInfo.count(); ++i)
+        for(IDList::const_iterator it = idList.begin(); it != idList.end(); ++it)
         {
-            FileInfo currentInfo = pDataDirectory->GetCompleteFileInfo(fileInfo[i]);
-            FileData currentData = m_dataStore.GetSingleFileData(currentInfo.ID);
+            FileInfo currentInfo = pDataDirectory->GetFileInfo(*it);
+            FileData currentData = m_dataStore.GetSingleFileData(*it);
             if (currentData.GetTable().size() == 0)
-            {
                 FilesForLoad.append(currentInfo);
-            }
         }
         load.loadData(FilesForLoad);
         //?????????????????;
-	return ITablePtr();
+    return ITablePtr();
 }
-
-CFileInfoList DataProvider::getContextsList()
-{
-    CFileInfoList results;
-    QVariantList devices = getFieldValuesCorrespondingToSelection(DEVICE);
-    QVariantList lots = getFieldValuesCorrespondingToSelection(LOT);
-    QVariantList wafers = getFieldValuesCorrespondingToSelection(WAFER);
-    QVariantList dates = getFieldValuesCorrespondingToSelection(DATE);
-    for (QVariantList::iterator itDev = devices.begin(); itDev != devices.end(); ++itDev)
-    {
-        for (QVariantList::iterator itL = lots.begin(); itL != lots.end(); ++itL)
-        {
-            for (QVariantList::iterator itW = wafers.begin(); itW != wafers.end(); ++itW)
-            {
-                for (QVariantList::iterator itDate = dates.begin(); itDate != dates.end();
-                     ++itDate)
-                {
-                        FileInfo currentFileInfo;
-                        currentFileInfo.m_date = (*itDate).toDateTime();
-                        currentFileInfo.m_fileContext.add(DEVICE,
-                                                          QPair<Value, FieldType> (*itDev,Context));
-                        currentFileInfo.m_fileContext.add(LOT,
-                                                          QPair<Value, FieldType> (*itL,Context));
-                        currentFileInfo.m_fileContext.add(WAFER,
-                                                          QPair<Value, FieldType> (*itW,Context));
-                        results.append(currentFileInfo);
-                }
-            }
-        }
-    }
-    return results;
-}
-
