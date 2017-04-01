76749ed43499704d2848d81b6493dcf295be5406
Anna-Vorskanyan <annavorskanyan@yahoo.com>
2017-04-01 21:46:30 +0400

    Update parser.h

--- src/parser.h
+++ src/parser.h
@@ -7,15 +7,29 @@
 #include <QFile>
 #include <QTextStream>
 
-class parser
+class CParser
 {
 private:
     QString m_filePath;
     static int NumberOfFiles;
 public:
-    parser(const QString&);
+    CParser(const QString&);
     FileInfo scanner();
     FileData loader(const FileType&);
+private:
+    void scanDefinition(QTextStream&, FileInfo&,QString);
+    void scanData(QTextStream&, FileInfo&, QString);
+    void scanParameterLimits(QTextStream&, FileInfo&);
+    void loadBinDefinition(QTextStream&, QVector<IVectorPtr>&,
+              TableInfo&);
+    void loadParameterDefinition(QTextStream&, QVector<IVectorPtr>&,
+              TableInfo&);
+    void loadBinData(QTextStream&, QVector<IVectorPtr>&,
+              TableInfo&);
+    void loadParameterData(QTextStream&, QVector<IVectorPtr>&,
+              TableInfo&);
+    void loadParameterLimits(QTextStream&, QVector<IVectorPtr>&,
+              TableInfo&);
 };
 
 #endif // PARSER_H
