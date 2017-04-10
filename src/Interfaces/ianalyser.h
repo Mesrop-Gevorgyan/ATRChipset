3fb3c5f76fe11efdd6049a6b504631111c7e6fef
TamaraSmbatyan <smbatyantamara95@gmail.com>
2017-04-10 14:22:20 +0400

    Update ianalyser.h

--- src/Interfaces/ianalyser.h
+++ src/Interfaces/ianalyser.h
@@ -10,12 +10,11 @@ class IAnalyser
 public:
 
     //init 
-    virtual void init(const IDataProvider* ,
-					  const CConfiguration&) = 0;
+    virtual void init(const IDataProvider*, const CConfiguration&) = 0;
     //returns the configuration
     virtual CConfiguration getConfig()const = 0;
-	//returns the Provider
-	virtual const IDataProvider* getProvider()const = 0;
+    //returns the Provider
+    virtual const IDataProvider* getProvider()const = 0;
     //runs the analyse
     virtual void run()const = 0;
 };
